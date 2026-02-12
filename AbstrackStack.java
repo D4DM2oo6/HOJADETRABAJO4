c30236582e6d46e32254d2b2b9d2eb3c7f8442e5
edven07 <ven25155@uvg.edu.gt>
2026-02-12 12:56:38 -0600

    ------------

--- AbstrackStack.java
+++ AbstrackStack.java
@@ -1,3 +1,14 @@
-public class AbstrackStack {
+public abstract class AbstrackStack<T> implements IStack<T> {
+    protected int size;
+    
+    @Override
+    public boolean isEmpty () {
+        return this.size == 0;
+    }
+    
+    @Override
+    public int size() {
+        return size;
+    }
     
 }
--- StackVector.java
+++ StackVector.java
@@ -1,6 +1,39 @@
-public interface  StackVector<T> {
-    void push (T value);
-    T pop ();
-    T peek ();
-    boolean isEmpty ();
+
+import java.util.Vector;
+
+public class  StackVector<T> extends AbstrackStack<T> {
+    
+    private Vector<T> vector;
+
+    public StackVector() {
+        this.vector = new Vector<>();
+    }
+
+    @Override
+    public void push(T value) {
+        data.add (item);
+        size++;
+    }
+
+     @Override
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        T value = data.remove(data.size() - 1);
+        size--;
+        return value;
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        return data.get(data.size() - 1);
+    }
+
+
 }
